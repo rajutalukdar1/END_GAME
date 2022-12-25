bc852e455392b561afc3db86a91a8838c5bcb516
ahadhasanjion <niallhoran3176@gmail.com>
2022-12-26 02:25:55 +0600

    Logo

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
-import { Box, Flex, HStack, IconButton, Stack, useDisclosure } from '@chakra-ui/react';
+import { Box, Flex, HStack, IconButton, Image, Stack, useDisclosure } from '@chakra-ui/react';
 import { Link, } from 'react-router-dom';
-
+import Logo from '../../../Assets/rsz_rsz_white-removebg-preview.png'
 const Navbar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const menuItems = <React.Fragment>
@@ -13,9 +13,9 @@ const Navbar = () => {
     </React.Fragment>
     return (
         <div className='list-none ff font-normal text-1xl'>
-            <Box  color='black' px={4}>
+            <Box color='black' px={4}>
                 <Flex alignItems={'center'} justifyContent={'space-between'}>
-                    <Flex h={16} alignItems={"center"}>
+                    <Flex h={16} alignItems={'center'}>
                         <IconButton
                             size={"sm"}
                             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -23,15 +23,16 @@ const Navbar = () => {
                             display={{ sm: "none" }}
                             onClick={isOpen ? onClose : onOpen}
                         />
-                        <HStack spacing={8} alignItems={"center"}>
-                            <Box ml={2}>SRJR</Box>
-                        </HStack>
+                        
+                          
+                          <Link to='/'><Image className="lg:ml-0 md:ml-0 ml-3" src={Logo} alt='Logo' /></Link>
+                        
                     </Flex>
                     <Flex alignItems={"center"}>
                         <HStack
                             as={"nav"}
                             spacing={4}
-                            display={{base:"none", md:"flex", sm:"flex"}}
+                            display={{ base: "none", md: "flex", sm: "flex" }}
                         >
                             {menuItems}
                         </HStack>
