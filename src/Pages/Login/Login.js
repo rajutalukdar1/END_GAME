50b709f2f88fe0f85c424fbcc9ca0dfd5da18a0b
rajutalukdar1 <lalutaluker@gmail.com>
2022-12-26 21:03:58 +0600

    login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,8 @@
-import { Box, Center, Circle, Text } from '@chakra-ui/react';
+import { Box, Button, Input, Circle, Text, WrapItem } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
@@ -13,37 +14,87 @@ const Login = () => {
     }
 
     return (
-        <div style={{height:'100%vh'}} className='h-[693px] flex justify-center items-center text-white lg:px-0 md:px-0 px-3 rounded'>
-            <div className='w-96  p-7 bg-green-600 rounded'>
-                <Circle mb={8}><Text as='b' fontSize='3xl'>Log in</Text></Circle>
-                <form onSubmit={handleSubmit(handleLogin)}>
-                    <Box className="form-control w-full max-w-xs mb-4">
-                        <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="text"
-                            {...register("email", {
-                                required: "Email Address is required"
-                            })}
-                            className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black" />
-                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
-                    </Box>
-                    <Box className="form-control w-full max-w-xs mb-4">
-                        <label className="label"> <span className="label-text">Password</span></label>
-                        <input type="password"
-                            {...register("password", {
-                                required: "Password is required",
-                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
-                            })}
-                            className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black" />
-                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
-                    </Box>
-                    <input className='btn btn-accent w-full text-xl pt-2' value="Login" type="submit" />
+        <div>
+            <div style={{ height: '100%vh' }} className='h-[693px] flex justify-center items-center text-black lg:px-0 md:px-0 px-3 rounded'>
+                <div className='w-96  p-7 bg-white shadow-2xl rounded'>
                     <div>
-                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                        <Circle mb={8}><Text as='b' fontSize='3xl'>Login</Text></Circle>
                     </div>
-                </form>
-                <Center pt={4}>New to SRJR ? <Link className='text-secondary ml-2' to="/signup">Sign Up</Link></Center>
-                <Circle>OR</Circle>
-                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <Box className="form-control w-full max-w-xs mb-4">
+                            <label className="label"> <span className="label-text">Email</span></label>
+                            <Input type="text"
+                                {...register("email", {
+                                    required: "Email Address is required"
+                                })}
+                                className="w-full max-w-xs py-2 rounded pl-4 text-black"
+                                isInvalid
+                                errorBorderColor='aqua.300'
+                                placeholder='Inter Your Email' />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                        </Box>
+                        <Box className="form-control w-full max-w-xs mb-4">
+                            <label className="label"> <span className="label-text">Password</span></label>
+                            <Input type="password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                })}
+                                className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black"
+                                isInvalid
+                                errorBorderColor='aqua.300'
+                                placeholder='Inter Your Email' />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </Box>
+                        <WrapItem>
+                            <Button className='w-[98%] mb-4' colorScheme='linkedin'><input value='Login' type="submit" /></Button>
+                        </WrapItem>
+
+                        {/* <input className='btn btn-accent w-full text-xl pt-2' value="Login" type="submit" /> */}
+                        <div>
+                            {loginError && <p className='text-red-600'>{loginError}</p>}
+                        </div>
+                        <small><p className='flex justify-center'>Don't have an accounts?<Link className='text-purple-600 font-bold' to='/signup'>Register now</Link></p></small>
+                    </form>
+
+                    {/* <Center pt={4}>New to SRJR ? <Link className='text-secondary ml-2' to="/signup">Sign Up</Link></Center> */}
+                    <p className='text-center'>-------------Or-------------</p>
+                    {/* onClick={handleSignInWithGoogle} */}
+                    <Link>
+                        <div className='flex justify-content-center align-items-center mt-3 '>
+                            <div className='flex justify-between items-center login-container hover:bg-warning'>
+                                <div className='w-8 h-8 ml-1'>
+                                    <img
+                                        src='https://i.ibb.co/7yz77Hj/google.png' alt=''
+                                    ></img>
+                                </div>
+                                <div className=' font-semibold '>
+                                    Continue with Google
+                                </div>
+                                <div className='mr-6'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className='flex justify-content-center align-items-center mt-3 '>
+                            <div className='flex justify-between items-center login-container hover:bg-warning'>
+                                <div className='w-8 h-8 ml-1'>
+                                    <img
+                                        src='https://i.ibb.co/Z62F8M5/github-512.png' alt=''
+                                    ></img>
+                                </div>
+                                <div className=' font-semibold '>
+                                    Continue with github
+                                </div>
+                                <div className='mr-6'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Signup/SignUp.js
+++ src/Pages/Signup/SignUp.js
@@ -1,4 +1,4 @@
-import { Box, Center, Circle, FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Circle, Input, Text, WrapItem } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
@@ -12,40 +12,97 @@ const SignUp = () => {
     }
 
     return (
-        <div className='h-[800px] flex justify-center items-center ff text-white'>
-            <div className='w-96 p-7 bg-green-600'>
-                <Circle mb={8}><Text as='b' fontSize='3xl'>Sign Up</Text></Circle>
-                <form onSubmit={handleSubmit(handleSignUp)}>
-                    <Box className="form-control w-full max-w-xs mb-4">
-                        <label className="label"> <span className="label-text">Name</span></label>
-                        <input type="text" {...register("name", {
-                            required: "Name is Required"
-                        })} className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black" />
-                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
-                    </Box>
-                    <Box className="form-control w-full max-w-xs mb-4">
-                        <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="email" {...register("email", {
-                            required: true
-                        })} className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black" />
-                        {errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
-                    </Box>
-                    <Box className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Password</span></label>
-                        <input type="password" {...register("password", {
-                            required: "Password is required",
-                            minLength: { value: 6, message: "Password must be 6 characters long" },
-                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
-                        })} className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black" />
-                        {errors.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>}
-                    </Box>
-                    <input className='btn btn-accent w-full mt-2 text-xl' value="Sign Up" type="submit" />
-                    {signUpError && <p className='text-red-600 text-sm'>{signUpError}</p>}
-                </form>
-                <Center pt={4}>Have an account ? <Link className='text-secondary' to="/login">Please Login</Link></Center>
-                <Center pt={2}>OR</Center>
-                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+        <div>
+            <div style={{ height: '100%vh' }} className='h-[693px] flex justify-center items-center text-black lg:px-0 md:px-0 px-3 rounded'>
+                <div className='w-96  p-7 bg-white shadow-2xl rounded'>
+                    <div>
+                        <Circle mb={8}><Text as='b' fontSize='3xl'>Signup Now</Text></Circle>
+                    </div>
+                    <form onSubmit={handleSubmit(handleSignUp)}>
+                        <Box className="form-control w-full max-w-xs mb-4">
+                            <label className="label"> <span className="label-text">Name</span></label>
+                            <Input type="text"
+                                {...register("name", {
+                                    required: "Name is required"
+                                })}
+                                className="w-full max-w-xs py-2 rounded pl-4 text-black"
+                                isInvalid
+                                errorBorderColor='aqua.300'
+                                placeholder='Inter Your Name' />
+                            {errors.name && <p className='text-red-600'>{errors.name?.message}</p>}
+                        </Box>
+                        <Box className="form-control w-full max-w-xs mb-4">
+                            <label className="label"> <span className="label-text">Email</span></label>
+                            <Input type="email"
+                                {...register("email", {
+                                    required: "Email Address is required"
+                                })}
+                                className="w-full max-w-xs py-2 rounded pl-4 text-black"
+                                isInvalid
+                                errorBorderColor='aqua.300'
+                                placeholder='Inter Your Email' />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                        </Box>
+                        <Box className="form-control w-full max-w-xs mb-4">
+                            <label className="label"> <span className="label-text">Password</span></label>
+                            <Input type="password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                })}
+                                className="input input-bordered w-full max-w-xs py-2 rounded pl-4 text-black"
+                                isInvalid
+                                errorBorderColor='aqua.300'
+                                placeholder='Inter Your Password' />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </Box>
+                        <WrapItem>
+                            <Button className='w-[98%] mb-4' colorScheme='linkedin'><input value='Signup' type="submit" /></Button>
+                        </WrapItem>
 
+                        {/* <input className='btn btn-accent w-full text-xl pt-2' value="Login" type="submit" /> */}
+                        <div>
+                            {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                        </div>
+                        <small><p className='flex justify-center'>Already have a accounts?<Link className='text-purple-600 font-bold' to='/login'>Login now</Link></p></small>
+                    </form>
+                    <p className='text-center'>-------------Or-------------</p>
+                    {/* onClick={handleSignInWithGoogle} */}
+                    <Link>
+                        <div className='flex justify-content-center align-items-center mt-3 '>
+                            <div className='flex justify-between items-center login-container hover:bg-warning'>
+                                <div className='w-8 h-8 ml-1'>
+                                    <img
+                                        src='https://i.ibb.co/7yz77Hj/google.png' alt=''
+                                    ></img>
+                                </div>
+                                <div className=' font-semibold '>
+                                    Continue with Google
+                                </div>
+                                <div className='mr-6'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link>
+                        <div className='flex justify-content-center align-items-center mt-3 '>
+                            <div className='flex justify-between items-center login-container hover:bg-warning'>
+                                <div className='w-8 h-8 ml-1'>
+                                    <img
+                                        src='https://i.ibb.co/Z62F8M5/github-512.png' alt=''
+                                    ></img>
+                                </div>
+                                <div className=' font-semibold '>
+                                    Continue with github
+                                </div>
+                                <div className='mr-6'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
